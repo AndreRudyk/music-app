068df0aec1f9edff0f4ebca7949634fdbfb1f477
Andre <andrii.rudyk01@gmail.com>
2025-02-16 10:21:26 +0200

    Adds docker containers for services

--- song-service/src/main/java/songservice/entity/SongMetadataEntity.java
+++ song-service/src/main/java/songservice/entity/SongMetadataEntity.java
@@ -2,6 +2,7 @@ package songservice.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,6 +11,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name = "song_metadata")
 public class SongMetadataEntity {
 
     @Id
